d7639bac437b21b7c050aa24745ade85818f0680
Robert Kuykendall <robert@rkuykendall.com>
2014-05-05 01:41:47 -0500

    Small updates.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
       <p>
         Texas State University &mdash; Computer Science B.Sc. and Honors Studies minor '12.
         <br /> Columbia University in the City of New York &mdash; Computer Science M.Sc. SEAS '15.
-        <br /> <a href="Kuykendall-CV.pdf">Resume</a> (pdf), last updated October 2013, and <a href="/design/">design sampling</a>.
+        <br /> <a href="Kuykendall-CV.pdf">Resume</a> (pdf), last updated October 2013</a>.
       </p>
         
       <p></p>
@@ -23,9 +23,9 @@
       <section>
         <h3>I love building web applications.</h3>
         <ul>
-          <li><a href="http://simplici7y.com">Simplici7y</a> &mdash; File sharing for the <a href="http://source.bungie.org">Aleph One</a> community. Written in Ruby on Rails in 2007, updated in 2011.</li>
-          <li><a href="http://dani365.com/years/2008/months/6">Dani 365</a> &mdash; Photo sharing site and 365 project. Written in Ruby on Rails in 2008.
-          <li><a href="http://www.pfhorums.com">The Pfhorums</a> &mdash; Forum for <a href="http://source.bungie.org">Aleph One</a> open source project, founded in 2005.</li>
+          <li><a href="http://simplici7y.com">Simplici7y</a> (<a href="https://github.com/rkuykendall/Simplici7y">github</a>) &mdash; File sharing for the <a href="http://source.bungie.org">Aleph One</a> community. Written in Ruby on Rails in 2007, updated in 2011.</li>
+          <li><a href="http://dani365.com/years/2008/months/6">Dani 365</a> &mdash; Photo sharing site and &ldquo;365&rdquo; photo-a-day project. Written in Ruby on Rails in 2008.
+          <li><a href="http://www.pfhorums.com">The Pfhorums</a> &mdash; Forum for <a href="http://source.bungie.org">Aleph One</a> open source project, founded in 2005, managed but not coded.</li>
         </ul>
       </section>
       
@@ -56,7 +56,7 @@
         <li>Phone &mdash; 
           &#43;&#49;&#32;&#40;&#50;&#49;&#48;&#41;&#32;&#50;&#48;&#52;&#45;&#56;&#51;&#57;&#49;
         </li>
-        <li>Address &mdash; 135th &amp; Broadway</li>
+        <li>Location &mdash; 135th &amp; Broadway</li>
         <li>AIM &mdash; 
           <script>
             <!--
@@ -91,11 +91,11 @@
     <section>
       <h3>You can also find me online.</h3>
       <ul>
+        <li>GitHub &mdash; <a href="https://github.com/rkuykendall">rkuykendall</a></li>
         <li>Facebook &mdash; <a href="https://www.facebook.com/robert.kuykendall">robert.kuykendall</a></li>
         <li>Hacker News &mdash; <a href="https://news.ycombinator.com/threads?id=rkuykendall-com">rkuykendall-com</a></li>
         <!-- li>Google Plus &mdash; <a href="https://plus.google.com/109526716594260505389/">Robert Kuykendall</a></li -->
         <!-- li>Twitter &mdash; <a href="https://twitter.com/rkuykendall">rkuykendall</a></li -->
-        <li>GitHub &mdash; <a href="https://github.com/rkuykendall">rkuykendall</a></li>
         <li>LinkedIn &mdash; <a href="http://www.linkedin.com/in/robertrkuykendall">robertrkuykendall</a></li>
         <li>Last.fm  &mdash; <a href="http://www.last.fm/user/Appleswitch">appleswitch</a></li>
         <li>Stack Overflow &mdash; <a href="http://stackoverflow.com/users/224873/robert-kuykendall">Robert Kuykendall</a></li>
@@ -104,14 +104,6 @@
       </ul>
     </section>
     
-        
-    <section style="display: none">
-      <h3><a href="https://twitter.com/rkuykendall" style="color: black">I am much better at using Facebook than Twitter.</a></h3>
-      <ul>
-        <?php // include("./twitter-rss/twitter-rss.php"); ?>
-      </ul>
-    </section>
-    		
-		</div>
+  </div>
     
 <?php print $close;
